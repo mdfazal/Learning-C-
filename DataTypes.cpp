f0cc72914ac08247ed5bc2838ea91cba8eb578f9
mdfazal <fazalmd964@gmail.com>
2021-03-23 15:20:24 +0530

    Learning Cpp

--- DataTypes.cpp
+++ DataTypes.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main()
 {
-    
+    char Grade = 'A';
+    string phrase = "Giraffe Academy";
+    int age = 50;
+    float fpa = 4.5;
+    double gpa = 5.5;
+
     return 0;
 }
