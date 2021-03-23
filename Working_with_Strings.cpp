0fbdf0d7ad588ec84dc08a4b25030fafe256f225
mdfazal <fazalmd964@gmail.com>
2021-03-23 15:53:43 +0530

    Learning Cpp

--- Working_with_Strings.cpp
+++ Working_with_Strings.cpp
@@ -4,5 +4,12 @@ using namespace std;
 
 int main()
 {
-    
+    cout << "Giraffe Academy \n";
+    cout << "Hello" <<endl;
+    string phrase = "Fazal is awesome";
+    cout << phrase;
+    cout << phrase.length();
+    cout << phrase[0];
+
+    return 0;
 }
