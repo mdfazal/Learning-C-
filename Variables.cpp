00f377016605eecd7de767ed27c6343ff1d1b15b
mdfazal <fazalmd964@gmail.com>
2021-03-23 15:02:13 +0530

    Learning Cpp

--- Variables.cpp
+++ Variables.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main()
 {
-    cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
+    cout << "There once was a man named George" << endl;
+    cout << "He was 70 Years old" << endl;
+    cout << "He liked the name George" << endl;
+    cout << "But did not like being 70" << endl;
 
     return 0;
 
