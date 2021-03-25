38e6121e562eb690afb76a37fe4050cd82e63192
mdfazal <fazalmd964@gmail.com>
2021-03-25 17:54:13 +0530

    Learning Cpp

--- 7_Geeting_Input.cpp
+++ 7_Geeting_Input.cpp
@@ -10,6 +10,21 @@ int main()
     cin >> age;
     cout << "You are " << age << " years old"; 
 
+    double age1;
+    cout << "Enter your age ";
+    cin >> age1;
+    cout << "You are " << age1 << " years old"; 
+
+    char age2;
+    cout << "Enter your age ";
+    cin >> age2;
+    cout << "You are " << age2 << " years old"; 
+
+    string name;
+    cout << "Enter your name ";
+    getline(cin,name); 
+    cout << "Hello " << name;
+
 
     return 0;
 }
