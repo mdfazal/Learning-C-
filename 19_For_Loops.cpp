659223b23c9379c2ea2cd625bd8b35499029043d
mdfazal <fazalmd964@gmail.com>
2021-03-25 21:38:05 +0530

    Learning Cpp

--- 19_For_Loops.cpp
+++ 19_For_Loops.cpp
@@ -10,5 +10,9 @@ int main()
     for(int i = 1; i <= 5; i++ ){
         cout << i << endl;
     }
+    int nums[] = {1,2,5,7,3};
+    for(int j = 0; j<5; j++){
+        cout << nums[j] << endl;
+    }
     return 0;
 }
