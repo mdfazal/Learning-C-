748ae6fe64b8bf8d516f534e40e68d1b16f1f675
mdfazal <fazalmd964@gmail.com>
2021-03-25 18:48:27 +0530

    Learning Cpp

--- 10_Arrays.cpp
+++ 10_Arrays.cpp
@@ -6,7 +6,11 @@ using namespace std;
 int main()
 {
     int luckyNums[] = {4,8,15,16,23,42};
-    
+    luckyNums[0] = 19;
     cout << luckyNums[0];
+
+    int luckyNum[20] = {4,8,15,16,23,42};
+    luckyNum[10] = 100;
+    cout << luckyNum[10];
     return 0;
 }
