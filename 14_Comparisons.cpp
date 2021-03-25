4edde4ae620fef6dac295e9427d489650737fe01
mdfazal <fazalmd964@gmail.com>
2021-03-25 20:19:42 +0530

    Learning Cpp

---
+++ 14_Comparisons.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int getMax(int num1, int num2){
+    int result;
+
+    if(num1 > num2)
+    {
+        result = num1;
+    }
+}
+int main()
+{
+
+    return 0;
+}
