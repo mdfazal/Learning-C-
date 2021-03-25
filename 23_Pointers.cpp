c35195ea1103f0894f8169a4c993483e94c7b429
mdfazal <fazalmd964@gmail.com>
2021-03-26 00:31:16 +0530

    Learning Cpp

--- 23_Pointers.cpp
+++ 23_Pointers.cpp
@@ -8,6 +8,9 @@ int main()
     int age = 19;
     double gpa = 3.5;
     string name = "Fazal";
-    
+
+    cout << "Age :" << &age << endl;
+    cout << "gpa :" << &gpa << endl;
+    cout << "Name :" << &name << endl;
     return 0;
 }
