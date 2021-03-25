51d30299bb7a48e63442b8ad3d4a9387a3cb950a
mdfazal <fazalmd964@gmail.com>
2021-03-26 01:40:22 +0530

    Learning Cpp

--- 27_Getters_and_Setters.cpp
+++ 27_Getters_and_Setters.cpp
@@ -3,22 +3,36 @@
 using namespace std;
 
 class Movie{
+    private:
+        string rating;
     public:
         string title;
         string director;
-        string rating;
         Movie(string aTitle, string aDirector, string aRating){
             title = aTitle;
             director = aDirector;
-            rating = aRating;
+            setRating(aRating);
         }
+
+        void setRating(string aRating){
+            if(aRating == "G" || aRating == "PG" || aRating == "PG-13" || aRating == "R" || aRating == "NR"){
+                rating = aRating;
+            } else {
+                rating = "NR";
+            }
+        }
+
+        string getRating(){
+            return rating;
+        }
+
 };
 
 int main()
 {
     Movie avengers("The Avengers", "Josh Whedon", "PG-13");
 
-    cout << avengers.rating;
+    cout << avengers.getRating();
 
     return 0;
 }
