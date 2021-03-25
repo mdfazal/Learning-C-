1815180c5c3b9e281a3e451e946606e465b3be43
mdfazal <fazalmd964@gmail.com>
2021-03-26 01:50:55 +0530

    Learning Cpp

--- 28_Inheritance.cpp
+++ 28_Inheritance.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Chef {
     public:
         void makeChicken(){
-            cout << "The chef makes chicken" << endl;
+            cout << "The chef makes yummy chicken" << endl;
         }
         void makeSalad(){
             cout << "The chef makes Salad" << endl;
@@ -15,10 +15,25 @@ class Chef {
         }
 };
 
+class ItalianChef : public Chef{
+    public:
+        void makePasta(){
+            cout << "The chef makes pasta" << endl;
+        }
+        void makeSpecialDish(){
+            cout << "The chef makes chicken parm" <<endl;
+        }
+};
+
 int main()
 {
     Chef chef;
     chef.makeChicken();
+    chef.makeSpecialDish();
+
+    ItalianChef italianchef;
+    italianchef.makePasta();
+    italianchef.makeSpecialDish();
 
     return 0;
 }
