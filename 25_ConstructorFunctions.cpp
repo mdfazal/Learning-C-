bf9c51f670cc333a640c3931ce9a0d345bdb044b
mdfazal <fazalmd964@gmail.com>
2021-03-26 01:01:39 +0530

    Learning Cpp

--- 25_ConstructorFunctions.cpp
+++ 25_ConstructorFunctions.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
 
-
 using namespace std;
+
+class book{
+    public:
+        string title;
+        string author;
+        int pages;
+
+    book(){
+        title = "No Title";
+        author = "no Author";
+        pages = 0;
+    }
+    
+    book(string aTitle, string aAuthor, int aPages){
+        title = aTitle;
+        author = aAuthor;
+        pages = aPages;
+    }
+};
 
 int main()
 {
-    
+    book book1("Harry Potter", "J.K.Rowling", 500);
+    book book2("Lord of the Rings", "Tolkien", 700);
+    book book3;
+    cout << book1.title;
+    cout << book3.title;
+  
     return 0;
+
 }
