4696f2fe85a915521d36a2a953f708c02671fea1
mdfazal <fazalmd964@gmail.com>
2021-03-25 20:09:43 +0530

    Learning Cpp

--- 12_Return_Statement.cpp
+++ 12_Return_Statement.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include <cmath>
 
+double cube(double num){
+    double result = num * num * num;
+    return result;
+}
+
 using namespace std;
 
 int main()
 {
+    double answer  = cube(5.0);
+    cout << answer;
+    return 0;
+}
