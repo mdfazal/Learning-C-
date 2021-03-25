6e3868bd46ea6b5316510b3b93fe0ba2e927ca61
mdfazal <fazalmd964@gmail.com>
2021-03-25 21:33:19 +0530

    Learning Cpp

---
+++ 18_Build_a_guessing_game.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    int secretNum = 7;
+    int guess;
+    int guessCount = 0;
+    int guessLimit = 3;
+    bool outOfGuesses = false;
+
+    while(secretNum != guess && !outOfGuesses){
+        if (guessCount < guessLimit){
+            cout << "Enter Guess :";
+            cin >> guess;
+            guessCount++;
+        } else {
+            outOfGuesses = true;
+        }
+        
+    }
+
+    if(outOfGuesses){
+        cout << "You lose";
+    } else {
+        cout << "You Win";
+    }
+    
+
+    return 0;
+}
