6e04a2ba90a1554e7bebc7f69f2cd04df7431314
mdfazal <fazalmd964@gmail.com>
2021-03-25 17:34:57 +0530

    Learning Cpp

--- 6_Working_with_Numbers.cpp
+++ 6_Working_with_Numbers.cpp
@@ -4,5 +4,13 @@ using namespace std;
 
 int main()
 {
-    cout << 40;
+    cout << 40 << endl;
+    cout << 5+7 << endl;
+    cout << 4*2 << endl;
+    cout << 4/2 << endl;
+    cout << 10%3 << endl;
+    int wnum = 5;
+    double dnum = 5.5;
+    cout << wnum ;
+    return 0;
 }
