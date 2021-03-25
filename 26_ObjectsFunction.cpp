26c69025cb7a2f5da1e8a697a0910e157951b160
mdfazal <fazalmd964@gmail.com>
2021-03-26 01:12:43 +0530

    Learning Cpp

--- 26_ObjectsFunction.cpp
+++ 26_ObjectsFunction.cpp
@@ -6,11 +6,18 @@ class student {
     public:
         string name;
         string major;
-        string gpa;
+        double gpa;
         student(string aName, string aMajor, double aGpa){
             name = aName;
             major = aMajor;
             gpa = aGpa;
+        }
+
+        bool hasHonors(){
+            if(gpa >= 3.5){
+                return true;
+            } 
+                return false;
         }
 };
 
@@ -18,6 +25,8 @@ int main()
 {
     student student1("Jim", "Business", 2.4);
     student student2("Pam", "Art", 3.6);
+
+    cout << student2.hasHonors();
 
     return 0;
 }
