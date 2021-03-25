9ccd3d8b1d1fa03e3f0cb1a7c7093058f968b92c
mdfazal <fazalmd964@gmail.com>
2021-03-25 18:38:55 +0530

    Learning Cpp

--- 9_Madlib.cpp
+++ 9_Madlib.cpp
@@ -5,3 +5,15 @@ using namespace std;
 
 int main()
 {
+    string color, pluralNoun, celebrity;
+
+    cout << "Enter a color ";
+    getline(cin, color);
+    cout << "Enter a plural noun ";
+    getline(cin, pluralNoun);
+    cout << "Enter a celebrity ";
+    getline(cin, celebrity);
+
+
+    return 0;
+}
