4fd69df54df77646d5ab8b42fedab4d44989e555
mdfazal <fazalmd964@gmail.com>
2021-03-25 20:23:22 +0530

    Learning Cpp

--- 14_Comparisons.cpp
+++ 14_Comparisons.cpp
@@ -3,20 +3,24 @@
 
 using namespace std;
 
-int getMax(int num1, int num2){
+int getMax(int num1, int num2, int num3){
     int result;
 
-    if(num1 > num2)
+    if(num1 >= num2 && num1>= num3)
     {
         result = num1;
     }
-    else
+    else if (num2 >= num1 && num2 >= num3)
     {
         result = num2;
+    }
+    else
+    {
+        result = num3;
     }
 }
 int main()
 {
-    cout << getMax(2, 5);
+    cout << getMax(2, 5, 10);
     return 0;
 }
