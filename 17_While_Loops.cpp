3f440af68864c5f94b84e9e6416ac7df19e23d5f
mdfazal <fazalmd964@gmail.com>
2021-03-25 21:26:30 +0530

    Learning Cpp

--- 17_While_Loops.cpp
+++ 17_While_Loops.cpp
@@ -5,6 +5,16 @@ using namespace std;
 
 int main()
 {
-    
+    int index = 1;
+    while (index <= 5){
+        cout << index << endl;
+        index++;
+    }
+     int ind = 6;
+     do{
+         cout << ind << endl;
+         ind++;
+     }while (ind <=5);
+
     return 0;
 }
