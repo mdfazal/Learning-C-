29aaaca85d2ddb158fd3b4fd39ba4ffeb0443921
mdfazal <fazalmd964@gmail.com>
2021-03-25 20:30:28 +0530

    Learning Cpp

---
+++ 15_Better_Calculator.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    char op;
+
+    cout << "Enter first number" << endl;
+    cin >> num1 ;
+    cout << "Enter operator" << endl;
+    cin >> op;
+    cout << "Enter second number " << endl;
+    cin >> num2;
+    int result;
+
+    if (op == '+')
+    {
+        result = num1 + num2;
+    }
+    else if (op == '-')
+    {
+        result = num1 - num2;
+    }
+    else if (op == '/')
+    {
+        result = num1 / num2;
+    }
+    else if (op == '*')
+    {
+        result = num1*num2;
+    }
+    else 
+    {
+        cout << "Invalid operator";
+    }
+
+    cout << result;
+}
