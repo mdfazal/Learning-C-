4fe96e4364facff6bf50e0ff2a44b093dd1ed50c
mdfazal <fazalmd964@gmail.com>
2021-03-25 22:58:47 +0530

    Learning Cpp

---
+++ 21_2DArray_NestedLoops.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    int numberGrid[][] = {
+                            {1,2},
+                            {3,4},
+                            {5,6}
+                        }
+
+    return 0;
+}
