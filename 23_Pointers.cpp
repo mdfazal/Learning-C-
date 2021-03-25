d2a489fe9b602a749c4f5e718b608cfdad1ee673
mdfazal <fazalmd964@gmail.com>
2021-03-26 00:34:10 +0530

    Learning Cpp

--- 23_Pointers.cpp
+++ 23_Pointers.cpp
@@ -6,8 +6,13 @@ using namespace std;
 int main()
 {
     int age = 19;
+    int *pAge = &age;
     double gpa = 3.5;
+    double *pGpa = &gpa;
     string name = "Fazal";
+    string *pName = &name;
+
+    cout << pAge << endl;
 
     cout << "Age :" << &age << endl;
     cout << "gpa :" << &gpa << endl;
