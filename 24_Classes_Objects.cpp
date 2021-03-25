8cd6615b990d8458cc06799726f18363d2aa5080
mdfazal <fazalmd964@gmail.com>
2021-03-26 00:49:20 +0530

    Learning Cpp

---
+++ 24_Classes_Objects.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+class book{
+    public:
+        string title;
+        string author;
+        int pages;
+};
+
+int main()
+{
+    book book1;
+    book1.title = "Harry Potter";
+    book1.author = "J.K.Rowling";
+    book1.pages = 500;
+
+    book book2;
+    book2.title = "Lord of the Rings";
+    book2.author = "Tolkien";
+    book2.pages = 700;
+
+    cout << book1.title;
+    cout << book1.pages;
+    cout << book2.title;
+    cout << book2.pages;
+    string name = "Fazal";
+    double pi = 3.14;
+    char favouriteLetter = 'G';
+    
+    return 0;
+
+}
