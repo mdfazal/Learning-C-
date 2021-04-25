9f42252422255ddfb10f0d61e111c4fe1176944e
mdfazal <fazalmd964@gmail.com>
2021-04-25 21:26:47 +0530

    Area program

--- 30_Area.cpp
+++ 30_Area.cpp
@@ -7,7 +7,7 @@ int main()
 {
     double area;
     int c;
-    float r;
+    float r, s, l , b;
     cout << "1. Area of a Circle";
     cout << "2.Area of a Square";
     cout << "3.Area of a rectangle";
@@ -20,6 +20,22 @@ int main()
         cin >> r;
         area = 3.14*r;
         cout << "Area of the circle is "<< area ;
+        break;
+
+    case 2:
+        cout << "Enter the side of the square";
+        cin >> s;
+        area = s*s;
+        cout << "Area of the square is "<< area ;
+        break;
+
+    case 3:
+        cout << "Enter the length of the rectangle";
+        cin >> l;
+        cout << "Enter the breadth of the rectangle";
+        cin >> b;
+        area = l*b;
+        cout << "Area of the rectangle is "<< area ;
         break;
     
     default:
