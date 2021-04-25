6a9f750c07f314a1e034ad125e9424a2d48514de
mdfazal <fazalmd964@gmail.com>
2021-04-25 21:21:29 +0530

    Area program

--- 30_Area.cpp
+++ 30_Area.cpp
@@ -5,5 +5,24 @@ using namespace std;
 
 int main()
 {
+    double area;
+    int c;
+    float r;
+    cout << "1. Area of a Circle";
+    cout << "2.Area of a Square";
+    cout << "3.Area of a rectangle";
+    cout << "Enter your Choice";
+
+    switch (c)
+    {
+    case 1:
+        cout << "Enter the radius of the circle";
+        cin >> r;
+        area = 3.14*r;
+        cout << "Area of the circle is "<< area ;
+        break;
     
+    default:
+        break;
+    }
 }
