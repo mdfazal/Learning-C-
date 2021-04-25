97e6f71792991642b540ff49b6af44ad82c2601e
mdfazal <fazalmd964@gmail.com>
2021-04-25 21:08:02 +0530

    Traingle program

--- 29_Triangle.cpp
+++ 29_Triangle.cpp
@@ -5,5 +5,15 @@ using namespace std;
 
 int main()
 {
-    
+    int a;
+    cout << "Enter your first side ";
+    cin >> a;
+
+    int b;
+    cout << "Enter your second side ";
+    cin >> b;
+
+    int c;
+    cout << "Enter your third side ";
+    cin >> c;
 }
