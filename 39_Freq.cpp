36c91c068865f63bcc27db1b084e94ac56908e14
mdfazal <fazalmd964@gmail.com>
2021-04-26 12:34:43 +0530

    Freq program

--- 39_Freq.cpp
+++ 39_Freq.cpp
@@ -5,9 +5,26 @@ using namespace std;
 
 int main()
 {
-    int a, num, sum=0, sum1=0;
-    int r;
-    int n;
+    int n, num;
     cout << "Enter a number" << endl;
     cin >> n;
+    int m = n;
+    int dig, p ;
+    for(int i = 1;i<=9;i++)
+        {
+            m = n;
+            int c=0;
+            while(m>0)
+            {
+                dig=m%10;
+                if(dig==i)
+                {
+                        c++;
+                        p=dig;
+                }
+                m/=10;
+                }
+                if(c>=1)
+                cout << "Frequency of " << i << "is" << c << endl;
+            }
 }
