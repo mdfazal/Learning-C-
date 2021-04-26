3dc247f043ef729bf23f8ecd3451b12ae6681de4
mdfazal <fazalmd964@gmail.com>
2021-04-26 11:31:42 +0530

    Armstrong program

---
+++ 37_Armstrong.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int d, sum=0, num;
+    int n;
+    cout << "Enter a numer" << endl;
+    cin >> n;
+    num = n;
+    while(num > 0)
+    {
+        d = (int)num%10;
+        sum = sum+d*d*d;
+        num = num/10;
+    }
+    if (n == sum)
+    {
+        cout << "Its a armstrong number";
+    }
+    else
+    {
+        cout << "Its not a armstrong number";
+    }
+}
