6e77437f31311b60c5d32a1b19d1c4daeb03d6da
mdfazal <fazalmd964@gmail.com>
2021-04-26 10:37:38 +0530

    Fibonacci program

--- 35_Fibonacci.cpp
+++ 35_Fibonacci.cpp
@@ -5,21 +5,20 @@ using namespace std;
 
 int main()
 {
-    int i, sum=0;
+    int a,b,c,d=3;
     int n;
     cout << "Enter a numer" << endl;
     cin >> n;
-    int a,b,c,d=3;
     a=0;
     b=1;
     c=0;
-    System.out.println("The fibonacci series is ");
-    System.out.println(a);
-    System.out.println(b);
+    cout << "The fibonacci series is ";
+    cout << a;
+    cout << b;
     while (d<=n)
     {
         c=a+b;
-        System.out.println(c);
+        cout << c;
         a=b;
         b=c;
         d=d+1;
