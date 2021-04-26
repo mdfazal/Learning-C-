bfa2e002944a4423d3368085be311f472826e5cf
mdfazal <fazalmd964@gmail.com>
2021-04-26 13:15:26 +0530

    Unique No program

--- 41_Unique.cpp
+++ 41_Unique.cpp
@@ -5,5 +5,33 @@ using namespace std;
 
 int main()
 {
-    int n, i;
+    int r1, r2, number, num1, num2, count = 0;  
+    cout << "Enter the number you want to check: ";   
+    cin >> number;
+    num1 = number;  
+    num2 = number;  
+
+    while (num1 > 0)  
+    {     
+        r1 = num1 % 10;  
+        while (num2 > 0)  
+        {   
+            r2 = num2 % 10;  
+ 
+            if (r1 == r2)  
+            {   
+                count++;  
+            }  
+            num2 = num2 / 10;  
+        }  
+        num1 = num1 / 10;  
+    }  
+    if (count == 1)  
+    {  
+        cout << "The number is unique.";  
+    }  
+    else  
+    {  
+    cout << "The number is not unique.";  
+    }  
 }
---
+++ 42_Automorphic.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int r1, r2, number, num1, num2, count = 0;  
+    cout << "Enter the number you want to check: ";   
+    cin >> number;
