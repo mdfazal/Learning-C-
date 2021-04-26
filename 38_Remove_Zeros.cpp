ecc137a391ee2e285a88b0d3fa89653ff1e9e0b5
mdfazal <fazalmd964@gmail.com>
2021-04-26 11:52:28 +0530

    Remove Zeros program

--- 38_Remove_Zeros.cpp
+++ 38_Remove_Zeros.cpp
@@ -5,7 +5,26 @@ using namespace std;
 
 int main()
 {
-    int d, sum=0, num;
+    int a, num, sum=0, sum1=0;
+    int r;
     int n;
     cout << "Enter a number" << endl;
     cin >> n;
+    num = n;
+    while(num>0)
+    {
+        a=num%10;
+        if(a!=0)
+        {
+            sum = sum *10 + a;
+        }
+        num=num/10;
+    }
+    while (sum>0)
+    {
+        r=sum%10;
+        sum1 = sum1 * 10+r;
+        sum=sum/10;
+    }
+    cout << sum1; 
+}
