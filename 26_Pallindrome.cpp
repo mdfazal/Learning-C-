ea1519930c18841d9cfbc326d83979b7f1bcacf3
mdfazal <fazalmd964@gmail.com>
2021-04-26 10:58:07 +0530

    Pallindrome program

---
+++ 26_Pallindrome.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int d, revnum, num;
+    int n;
+    cout << "Enter a numer" << endl;
+    cin >> n;
+    num = n;
+    while(num > 0)
+    {
+        d = (int)num%10;
+        revnum = revnum*10+d;
+        num = num/10;
+    }
+    if(revnum == n)
+    {
+        cout << "This is a pallindrome number";
+    }
+    else
+    {
+        cout << "This is not a Pallindrome number";
+    }
+}
