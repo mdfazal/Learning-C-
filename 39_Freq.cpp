d220e253554d605673dfd57362089322d15bf56a
mdfazal <fazalmd964@gmail.com>
2021-04-26 11:57:58 +0530

    Freq program

---
+++ 39_Freq.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int a, num, sum=0, sum1=0;
+    int r;
+    int n;
+    cout << "Enter a number" << endl;
+    cin >> n;
+}
